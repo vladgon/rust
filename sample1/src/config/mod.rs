f9177aa44b9dca520f598d2d5284d383ac84da6e
vladgon <valdgon@gmail.com>
2019-01-21 22:17:09 -0500

    Added config

--- sample1/src/config/mod.rs
+++ sample1/src/config/mod.rs
@@ -1,18 +1,19 @@
-use config::model::config_model::*;
-pub use config::model::config_model::ENV as Env;
-use util;
+use config::model::*;
+pub use config::model::ENV as Env;
+
+//use util;
 
 pub mod model;
 
 pub fn load(env: ENV) -> Result<Config, serde_json::Error> {
-    return match env {
+    match env {
         ENV::DEV => serde_json::from_str::<Config>(include_str!("app_dev.json")),
         ENV::PROD => serde_json::from_str::<Config>(include_str!("app_prod.json"))
-    };
+    }
 }
 
 #[test]
-fn load_config() -> util::ResultOK {
-    assert_eq!(serde_json::from_str::<Config>(include_str!("app_dev.json"))?.app_name.len() > 0, true);
+fn load_config() -> crate::util::ResultOK {
+    assert!(serde_json::from_str::<Config>(include_str!("app_dev.json"))?.app_name.len() > 0);
     Ok(())
 }
--- sample1/src/config/model/mod.rs
+++ sample1/src/config/model/mod.rs
@@ -1,1 +1,18 @@
-pub mod config_model;
+#[derive(Debug, Serialize, Deserialize, Default)]
+pub struct Config {
+    pub app_name: String,
+    pub db_user: String,
+    pub db_pwd: String,
+    pub dp_prop: DbProp,
+}
+
+#[derive(Debug, Serialize, Deserialize, Default)]
+pub struct DbProp {
+    db_name: String
+}
+
+#[derive(Debug)]
+pub enum ENV {
+    DEV,
+    PROD,
+}
