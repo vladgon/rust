1b2ee7890a548606c297c74f49386df7e72ec195
vova <vladgon@gmail.com>
2024-01-23 12:43:54 -0500

    added proto wkt

--- wspace/wg_proto/build.rs
+++ wspace/wg_proto/build.rs
@@ -1,34 +1,77 @@
 use std::env;
+use std::path::PathBuf;
+
+use prost_build::Config;
+use prost_wkt_build::{FileDescriptorSet, Message};
+
+use wg_util::info_build;
 
 const PROTO_ROOT: &str = "proto";
-const PROTO_PACKAGE: &str = "wg.proto.model";
-
-static SERDE_NAMES: &[&str] = &[
-    "Shirt",
-    "Foo1"
-];
+const PROTO_PACKAGE: &str = "proto.model";
 
 static FILE_NAMES: &[&str] = &[
     "sample",
     "sample1"
 ];
+/// Custom name replace tuple (\<JSON Path\>, \<New Name\>)
+static JSON_NAMES: &[(&str, &str)] = &[
+    ("Shirt.color", "color_custom")
+];
 
-fn main() -> std::io::Result<()> {
-    env::set_var("ARGO_PROFILE_DEV_BUILD_OVERRIDE_DEBUG", "true");
+fn main() -> wg_util::Result<()> {
+    env::set_var("CARGO_PROFILE_DEV_BUILD_OVERRIDE_DEBUG", "true");
     println!("cargo:rerun-if-changed=proto");
-    let config = &mut prost_build::Config::new();
-    SERDE_NAMES
+    let config = &mut Config::new();
+    let config = JSON_NAMES
         .iter()
         .fold(config,
-              |config, &name|
-                  config.type_attribute(with_pkg(name), "#[derive(serde::Serialize, serde::Deserialize)]")
-                      .type_attribute(with_pkg(name), "#[serde(default)]"),
+              |config, &t2|
+                  config.field_attribute(t2.0,
+                                         format!(r##"#[serde(rename(serialize = "{}", deserialize = "{}"))]"##,
+                                                 t2.1,
+                                                 t2.1
+                                         )));
+    _ = wkt(config)?;
+    Ok(())
+}
+
+fn wkt(config: &mut Config) -> wg_util::Result<&mut Config> {
+    let out = PathBuf::from(env::var("OUT_DIR")?);
+    let descriptor_file = out.join("descriptors.bin");
+    config
+        .type_attribute(
+            ".",
+            "#[derive(serde::Serialize,serde::Deserialize)]",
         )
+        .extern_path(
+            ".google.protobuf.Any",
+            "::prost_wkt_types::Any",
+        )
+        .extern_path(
+            ".google.protobuf.Timestamp",
+            "::prost_wkt_types::Timestamp",
+        )
+        .extern_path(
+            ".google.protobuf.Value",
+            "::prost_wkt_types::Value",
+        )
+        .file_descriptor_set_path(&descriptor_file)
+        .include_file({
+            info_build!("Generating include file: {}/{PROTO_PACKAGE}.include.rs",out.to_str().unwrap());
+            format!("{PROTO_PACKAGE}.include.rs")
+        })
         .compile_protos(FILE_NAMES.iter()
                             .map(|name| format!("{PROTO_ROOT}/{name}.proto"))
                             .collect::<Vec<String>>()
                             .as_ref(),
-                        &[format!("{}/", PROTO_PACKAGE)])
-}
+                        &[format!("{}/", PROTO_PACKAGE)])?;
+    wg_util::info_build!("Generating WKT descriptor_file:  {:?}",descriptor_file);
 
-fn with_pkg(name: &str) -> String { format!("{PROTO_PACKAGE}.{name}") }
+
+    let descriptor_bytes = std::fs::read(descriptor_file)?;
+
+    let descriptor = FileDescriptorSet::decode(&descriptor_bytes[..])?;
+
+    prost_wkt_build::add_serde(out, descriptor);
+    Ok(config)
+}
--- wspace/wg_proto/src/main.rs
+++ wspace/wg_proto/src/main.rs
@@ -1,32 +1,33 @@
+use chrono::Utc;
 use log::debug;
 use prost::Message;
+use prost_wkt_types::Timestamp;
 
 use wg_util::common::config::log::LogDefaults;
 use wg_util::common::config::rust_app;
 use wg_util::Result;
 
-use crate::wg::proto::model;
+use crate::proto::model;
 
-pub mod wg {
-    pub mod proto {
-        pub mod model {
-            include!(concat!(env!("OUT_DIR"), "/wg.proto.model.rs"));
-        }
-    }
-}
+include!(concat!(env!("OUT_DIR"), "/proto.model.include.rs"));
 
 fn main() -> Result<()> {
     rust_app::init(LogDefaults::default(), false)?;
 
     let mut shirt = model::Shirt::default();
-    let mut foo1 = model::Foo1::default();
     shirt.color = Some("Green".to_string());
+    shirt.size = model::shirt::Size::Large.into();
     shirt.set_size(model::shirt::Size::Large);
+    shirt.created_on = Some(Timestamp::from(Utc::now()));
     let res: Vec<u8> = shirt.encode_to_vec();
     debug!("Shirt {:?}", res);
     debug!("Shirt {:?}", serde_json::to_string(&shirt)?);
-    foo1.name = "foo1".to_string();
-    foo1.set_types(model::PhoneType1::Mobile1);
-    debug!("{} {:?}", "ProtoSchema::",  serde_json::to_string(&foo1)?);
+
+    let foo = model::Foo {
+        name: str::to_string("foo"),
+        types: model::PhoneType::Home1.into(),
+        ..Default::default()
+    };
+    debug!("{} {:?}", "ProtoSchema::",  serde_json::to_string(&foo)?);
     Ok(())
 }
